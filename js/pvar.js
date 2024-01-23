5e0e9985ede032363d5616e86dc586097951326a
imagineskies <weeden.rayna@outlook.com>
2024-01-23 07:54:05 -0500

    Finished Poems Redesign

--- js/pvar.js
+++ js/pvar.js
@@ -12,8 +12,14 @@ var homePageMenu = `
 <li class="nav-item">
     <a class="nav-link" href="#">Home</a>
 </li>
-<li class="nav-item">
-    <a class="nav-link" href="Pages/writings.html">Writings</a>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Writings
+    </a>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="Pages/poems.html">Poems</a></li>
+        <li><a class="dropdown-item" href="#">Infrequents</a></li>
+    </ul>
 </li>
 <li class="nav-item">
     <a class="nav-link" href="Pages/sandbox.html">Sandbox</a>
@@ -25,8 +31,14 @@ var pagesMenu = `
 <li class="nav-item">
     <a class="nav-link" href="../index.html">Home</a>
 </li>
-<li class="nav-item">
-    <a class="nav-link" href="writings.html">Writings</a>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Writings
+    </a>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="poems.html">Poems</a></li>
+        <li><a class="dropdown-item" href="#">Infrequents</a></li>
+    </ul>
 </li>
 <li class="nav-item">
     <a class="nav-link" href="sandbox.html">Sandbox</a>
@@ -35,23 +47,23 @@ var pagesMenu = `
 var pagesMenuClasses = "navbar-nav ms-auto mb-2 mb-lg-0"
 
 var navbarsPoemPages = `
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsPP" aria-controls="navbarsPP" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarsPP">
-        <a class="navbar-brand siteTitle" href="#">Pulcherrimum Quaerere</a>
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="../../index.html">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../writings.html">Writings</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../sandbox.html">Sandbox</a>
-                </li>
-            </ul>
-        </div>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsPP" aria-controls="navbarsPP" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarsPP">
+<a class="navbar-brand siteTitle" href="#">Pulcherrimum Quaerere</a>
+<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+<li class="nav-item">
+<a class="nav-link" href="../../index.html">Home</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="../writings.html">Writings</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="../sandbox.html">Sandbox</a>
+</li>
+</ul>
+</div>
 `;
 
 
